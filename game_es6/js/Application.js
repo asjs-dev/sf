153f673d210ddf7692648c7f309de40f47558e0b
asjs-dev <kormendi.gergely@gmail.com>
2020-11-02 11:33:13 +0100

    Remove self invoking function from Application

--- game_es6/js/Application.js
+++ game_es6/js/Application.js
@@ -7,66 +7,64 @@ import MenuViewController from "./viewController/MenuViewController.js";
 import StatusViewController from "./viewController/StatusViewController.js";
 import SplashViewController from "./viewController/SplashViewController.js";
 
-(function() {
-  class Application extends BaseClass {
-    constructor() {
-      super();
+class Application extends BaseClass {
+  constructor() {
+    super();
 
-      this._dataObserver = new DataObserver();
+    this._dataObserver = new DataObserver();
 
-      this._baseTexture;
+    this._baseTexture;
 
-      this._renderer;
+    this._renderer;
 
-      this._views = [];
+    this._views = [];
 
-      this._updateBound = this.update.bind(this);
-      document.addEventListener("DOMContentLoaded", this._onDOMContentLoad.bind(this));
-    }
+    this._updateBound = this.update.bind(this);
+    document.addEventListener("DOMContentLoaded", this._onDOMContentLoad.bind(this));
+  }
 
-    update() {
-      let state = this._dataObserver.getState();
+  update() {
+    let state = this._dataObserver.getState();
 
-      state.time = Date.now();
+    state.time = Date.now();
 
-      for (let i = 0, l = this._views.length; i < l; ++i) this._views[i].update(state);
+    for (let i = 0, l = this._views.length; i < l; ++i) this._views[i].update(state);
 
-      this._dataObserver.setState(state);
+    this._dataObserver.setState(state);
 
-      requestAnimationFrame(this._updateBound);
-    }
+    requestAnimationFrame(this._updateBound);
+  }
 
-    _setup() {
-      this._baseTexture = PIXI.BaseTexture.from("img/assets.png");
-      this._baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
+  _setup() {
+    this._baseTexture = PIXI.BaseTexture.from("img/assets.png");
+    this._baseTexture.scaleMode = PIXI.SCALE_MODES.NEAREST;
 
-      this._renderer = new Renderer(
-        this._baseTexture,
-        {
-          width           : Config.size.width,
-          height          : Config.size.height,
-          backgroundColor : 0x000000
-        }
-      );
+    this._renderer = new Renderer(
+      this._baseTexture,
+      {
+        width           : Config.size.width,
+        height          : Config.size.height,
+        backgroundColor : 0x000000
+      }
+    );
 
-      this._views.push(new GameViewController(this._dataObserver, this._renderer, this._baseTexture));
-      this._views.push(new StatusViewController(this._dataObserver));
-      this._views.push(new MenuViewController(this._dataObserver));
-      this._views.push(new SplashViewController(this._dataObserver));
+    this._views.push(new GameViewController(this._dataObserver, this._renderer, this._baseTexture));
+    this._views.push(new StatusViewController(this._dataObserver));
+    this._views.push(new MenuViewController(this._dataObserver));
+    this._views.push(new SplashViewController(this._dataObserver));
 
-      this._dataObserver.setState(Config.defaultState);
+    this._dataObserver.setState(Config.defaultState);
 
-      this.update();
-    }
+    this.update();
+  }
 
-    _loadAssets() {
-      PIXI.Loader.shared.add("img/assets.png").load(this._setup.bind(this));
-    }
+  _loadAssets() {
+    PIXI.Loader.shared.add("img/assets.png").load(this._setup.bind(this));
+  }
 
-    _onDOMContentLoad() {
-      this._loadAssets();
-    }
-  };
+  _onDOMContentLoad() {
+    this._loadAssets();
+  }
+};
 
-  new Application();
-})();
+new Application();
